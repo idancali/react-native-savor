f0c170ed873dbeac1bb0e701098a7a12462a4c19
I. Dan Calinescu <idancalinescu@gmail.com>
2017-02-22 11:51:14 +0200

    using react version

--- src/main.js
+++ src/main.js
@@ -1,32 +1,9 @@
 'use strict'
 
 /**
- *  We're going to start off with a vanilla savor install
- *  that we will extend to work with React Native apps
+ *  We're going to start off with a vanilla react savor install
+ *  that we will extend to work with React Native apps, if necessary
  **/
-var savor = require("savor");
+var savor = require("react-savor")
 
-/**
- *  We're using Chai.JS as our BDD assertion library, and specifically,
- *  we want to write our specs using the expect assert style.
- **/
-var chai = require('chai');
-
-/**
- *  Use helpers for testing React
- **/
-var enzyme = require('enzyme');
-var chaiEnzyme = require('chai-enzyme');
-chai.use(chaiEnzyme())
-
-
-/**
- *   Extend the root context to include enzyme tools
- **/
-savor.context = Object.assign(savor.context, {
-  mount: enzyme.mount,
-  render: enzyme.render,
-  shallow: enzyme.shallow
-});
-
-module.exports = savor;
+module.exports = savor
