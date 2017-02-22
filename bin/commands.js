f3a4deaa97cbef9231febb25b0cad16b002443de
I. Dan Calinescu <idancalinescu@gmail.com>
2017-02-22 11:32:04 +0200

    added registering options

--- bin/commands.js
+++ bin/commands.js
@@ -3,9 +3,7 @@ var coverageBin = "./node_modules/.bin/babel-node";
 var coverageLocalArgs = ['./node_modules/.bin/istanbul', 'cover', './node_modules/.bin/_mocha', '--', '--require', 'react-native-mock/mock', '--require', './node_modules/react-native-savor/res/includes.js', '--report', 'lcovonly', 'test/specs/**/*.js'];
 
 var testBin = "./node_modules/.bin/mocha"
-// var testArgs = ['--require', 'react-native-mock/mock', '--require', './node_modules/react-native-savor/res/includes.js', '--compilers', 'js:babel-core/register', '--recursive', 'test/specs/**/*.js']
-var testArgs = ['--require', 'react-native-mock/mock', '--require', './test/indedx.js', '--compilers', 'js:babel-core/register', '--recursive', 'test/specs/**/*.js']
-// var testArgs = ['--require', 'react-native-mock/mock', '--compilers', 'js:babel-core/register', '--recursive', 'test/specs/**/*.js']
+var testArgs = ['--require', 'react-native-mock/mock', '--require', './node_modules/react-native-savor/res/includes.js', '--compilers', 'js:babel-core/register', '--recursive', 'test/specs/**/*.js']
 
 commands.test = [{
   bin: testBin,
--- res/includes.js
+++ res/includes.js
@@ -1,24 +1,9 @@
 const path = require('path')
 
-var deps = []
+var options = {}
 
 try {
-  const mod = require(path.resolve(process.cwd(), 'test'))
-  if (mod.deps) {
-    deps = deps.concat(mod.deps)
-  }
+  options = Object.assign(options, require(path.resolve(process.cwd(), 'test')))
 } catch (e) { }
 
-
-function isDep(file) {
-  var found = false
-  deps.forEach(dep => {
-    const full = path.resolve("node_modules", dep)
-    found = file.startsWith(full)
-  })
-  return found
-}
-
-require('babel-register')({
-  ignore: (file) => !isDep(file)
-})
+require('babel-register')(options)
