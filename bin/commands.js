f514431a500babdb0ff8225ea9cae66634d15634
I. Dan Calinescu <idancalinescu@gmail.com>
2017-02-22 09:36:56 +0200

    included dep transpiling

--- bin/commands.js
+++ bin/commands.js
@@ -1,6 +1,6 @@
 var commands = require('savor/bin/commands')
 var coverageBin = "./node_modules/.bin/babel-node";
-var coverageLocalArgs = ['./node_modules/.bin/istanbul', 'cover', './node_modules/.bin/_mocha', '--', '--require', 'react-native-mock/mock', '--report', 'lcovonly', 'test/specs/**/*.js'];
+var coverageLocalArgs = ['./node_modules/.bin/istanbul', 'cover', './node_modules/.bin/_mocha', '--', '--require', 'react-native-mock/mock', '--require', './node_modules/react-native-savor/res/includes.js', '--report', 'lcovonly', 'test/specs/**/*.js'];
 
 var testBin = "./node_modules/.bin/mocha"
 var testArgs = ['--require', 'react-native-mock/mock', '--require', './node_modules/react-native-savor/res/includes.js', '--compilers', 'js:babel-core/register', '--recursive', 'test/specs/**/*.js']
--- res/includes.js
+++ res/includes.js
@@ -1,9 +1,24 @@
 const path = require('path')
 
-require('babel-register')({
-  ignore: function(filename) {
-    const full = path.resolve('./node_modules/react-chunky')
-    const isDep = filename.startsWith(full)
-    return !isDep
+var deps = ["react-native-savor"]
+
+try {
+  const mod = require(path.resolve(process.cwd(), 'test'))
+  if (mod.deps) {
+    deps = deps.concat(mod.deps)
   }
+} catch (e) { }
+
+
+function isDep(file) {
+  var found = false
+  deps.forEach(dep => {
+    const full = path.resolve("node_modules", dep)
+    found = file.startsWith(full)
+  })
+  return found
+}
+
+require('babel-register')({
+  ignore: !isDep
 })
